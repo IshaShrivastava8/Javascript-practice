16bbdda90dd1b85b83d8b8a55268f1c43ec0ebdd
Isha Shrivastava <isha8801@gmail.com>
2025-03-23 23:03:54 +0530

    prac

--- 02datatypes.js
+++ 02datatypes.js
@@ -1,6 +1,9 @@
 1"use strict"  // treat all JS code as newer version
 
 alert(3+3) // we are using node js not browser
+
 //code readbility should be high
+
 //mdn documentation
-//tc39.es
+ 
+//tc39.es 3
